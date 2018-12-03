93a56c333ce3a12d3aaf9c780ca6fcc6d15766b3
jfebrerc <jfebrerc@gmail.com>
2018-12-03 15:56:00 +0100

    PETADETA.negativet

--- src/Gestio/Assignacio_EA/Registrar_assignacio.java
+++ src/Gestio/Assignacio_EA/Registrar_assignacio.java
@@ -1,6 +1,7 @@
 package Gestio.Assignacio_EA;
 
 import Biblioteques.Auxiliar;
+import Biblioteques.IO;
 
 import javax.swing.*;
 import java.awt.event.ComponentAdapter;
@@ -17,6 +18,8 @@ public class Registrar_assignacio {
     private JTextField buscarAtraccions;
     private JButton buscarAButton;
     private JPanel registrarAssignacio;
+    private JButton seleccionarEmpleat;
+    private JButton seleccionarAtraccio;
     private static JFrame frame_menuRegistrarAssignacio = new JFrame("registrarAssignacio");
 
     public Registrar_assignacio() {
@@ -25,6 +28,24 @@ public class Registrar_assignacio {
             public void componentResized(ComponentEvent e) {
                 super.componentResized(e);
                 Auxiliar.llistar_Atraccions(buscarAtraccions, llistaAtraccions);
+            }
+        });
+        llistaEmpleats.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                super.componentResized(e);
+                Auxiliar.llistar_empleats(buscarEmpleats, llistaEmpleats);
+                Object indices2=llistaEmpleats.getSelectedValue();
+                IO.imprimirTI("Contingut: "+ (indices2));
+                if (indices2 == null){
+                    //JOptionPane.showMessageDialog(frame_menuRegistrarAssignacio, "Selecciona un empleat");
+                }else {
+                    try {
+
+                    }catch (Exception error){
+                        IO.imprimirTI("Error al llistar empleats a assignacions: " + error);
+                    }
+                }
             }
         });
     }
